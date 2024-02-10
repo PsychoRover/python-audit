828733fcee6738c0a4508a768399a548c105ae74
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2024-02-10 21:34:00 +0000

    Update Environment
    Remove `README.md` from `pyproject.toml`.
    Amend auditd install command.

--- scripts/install-dev-tools.sh
+++ scripts/install-dev-tools.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install bash-completion
-sudo apt-get install auditd
+sudo apt-get -y install auditd
 sudo chmod -R u+rw /var/log/audit
 sudo chmod -R u+rw /etc/audit
 poetry install
