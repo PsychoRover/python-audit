77448670b34edcbc270ebea251392ab505955716
PsychoRover <69534855+PsychoRover@users.noreply.github.com>
2024-02-10 21:00:38 +0000

    Add Logs
    Added `loguru` package.
    Added logs to main function.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import glob
 import pathlib
 
 from db import create_db_and_tables, engine
+from loguru import logger
 from sqlmodel import Session
 
 from src.constants import DATABASE, LOG_GLOB_PATTERN
@@ -9,11 +10,14 @@ from src.log_parser import parse_log
 
 
 def main():
+    logger.debug(f"{'Initializing Database':*^50}")
     pathlib.Path(DATABASE).unlink(missing_ok=True)
     create_db_and_tables()
 
+    logger.debug(f"{'Getting Auditd Logs':*^50}")
     logs = glob.glob(LOG_GLOB_PATTERN)
 
+    logger.debug(f"{'Running Parser...':*^50}")
     for log in logs:
         messages = parse_log(log)
         with Session(engine) as session:
